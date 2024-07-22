d17b7330836e3ff102d3c21e5bbde688049cdd00
BRANDON_GARCIA <159663607+BrandonGS22b@users.noreply.github.com>
2024-07-22 10:38:38 -0500

    creacion del servidor , puerto,.env, cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.develop.env`,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import { CORS } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(morgan('dev'));
+
+  const configService = app.get(ConfigService);
+  app.enableCors(CORS); //llamamos el cors de index de la carpeta constant
+  app.setGlobalPrefix('api'); //ahora le ponemos esto en postman para hacer consultas localhost:8000/api/users/say-hello
+
+  await app.listen(configService.get('PORT'));
+  console.log('llamando al puerto: ' + configService.get('PORT')); //para ver el puerto por consola y ver que esta levantado
+  console.log(`Application running on: ${await app.getUrl()}`); //para ver el puerto en consola mediante un link
 }
 bootstrap();
