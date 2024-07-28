568af63495dfae6d0e40c09e7ec5314b5ba02f0f
BRANDON_GARCIA <159663607+BrandonGS22b@users.noreply.github.com>
2024-07-27 20:05:52 -0500

    creando atributos de la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceConfig } from './config/data.source';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
   imports: [
@@ -8,7 +11,9 @@ import { UsersModule } from './users/users.module';
       isGlobal: true,
       envFilePath: `.develop.env`,
     }),
+    TypeOrmModule.forRoot({ ...DataSourceConfig }), //para pasarlo sin los puntos ya que lo estoy mandando como objeto le quito los puntos y llaves
     UsersModule,
+    ProjectsModule,
   ],
 })
 export class AppModule {}
--- src/config/data.source.ts
+++ src/config/data.source.ts
@@ -1,13 +1,30 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
+/*
+PORT=8000
+
+DB_HOST = localhost
+DB_PORT=5432
+DB_USER= ucodrr
+DB_PASSWORD = secret1234
+DB_NAME = codrrdb
+*/
+
+ConfigModule.forRoot({
+  isGlobal: true,
+  envFilePath: `.develop.env`,
+});
+const configService = new ConfigService();
+
 export const DataSourceConfig: DataSourceOptions = {
   type: 'postgres', // Especifica el tipo de base de datos, en este caso PostgreSQL.
-  host: '', // La dirección del host de la base de datos.
-  port: 0, // El puerto en el que la base de datos está escuchando.
-  username: '', // El nombre de usuario para la autenticación en la base de datos.
-  password: '', // La contraseña para la autenticación en la base de datos.
-  database: '', // El nombre de la base de datos.
+  host: configService.get('DB_HOST'), // La dirección del host de la base de datos.
+  port: configService.get('DB_PORT'), // El puerto en el que la base de datos está escuchando.
+  username: configService.get('DB_USER'), // El nombre de usuario para la autenticación en la base de datos.
+  password: configService.get('DB_PASSWORD'), // La contraseña para la autenticación en la base de datos.
+  database: configService.get('DB_NAME'), // El nombre de la base de datos.
   entities: [__dirname + '/../**/*.entity{.ts,.js}'], // La ruta a las entidades (clases que representan las tablas en la base de datos).
   migrations: [__dirname + '/../migrations/*{.ts,.js}'], // La ruta a los archivos de migración.
   synchronize: false, // Si `true`, TypeORM sincroniza la base de datos con las entidades cada vez que se conecta. `false` es más seguro en producción.
